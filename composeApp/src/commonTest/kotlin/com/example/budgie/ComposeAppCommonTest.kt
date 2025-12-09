a713a94b3c6ed3b3ff6547815546c21ea8472485
Ben Morley <bmorley12@protonmail.com>
2025-12-09 10:26:40 -0700

    Initial commit

---
+++ composeApp/src/commonTest/kotlin/com/example/budgie/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.example.budgie
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
